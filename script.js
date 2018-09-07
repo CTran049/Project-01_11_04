e083e1b26d0cd9175079c787fadb5f200bb4842c
Unknown <ctran049@west-mec.org>
2018-09-07 15:24:41 -0700

    Added Code

--- script.js
+++ script.js
@@ -1,10 +1,103 @@
 /*  Project 01_11_04
 
-    Author: 
-    Date:   
+    Author: Charley Tran
+    Date: 9.7.18   
 
     Filename: script.js
 */
 
 "use strict";
 
+// Global Variables
+var httpRequest = false;
+var countrySel;
+
+// Checks to see if buttons are selected
+function checkButtons() {
+    var germany = document.getElementById("germany");
+    var us = document.getElementById("us");
+
+    if (germany.checked || us.checked) {
+        document.getElementById("zipset").style.visibility = "visible";
+        if (germany.checked) {
+            countrySel = "de";
+        } else {
+            countrySel = "us";
+        }
+    }
+}
+
+// Checks to see if there's a value in the input.
+function checkInput() {
+    var zip = document.getElementById("zip").value;
+    if (zip.length === 5) {
+        getLocation()
+    } else {
+        document.getElementById("city").value = "";
+        document.getElementById("state").value = "";
+    }
+}
+
+function getRequestObject() {
+    try {
+        httpRequest = new XMLHttpRequest();
+    } catch (requestError) {
+        document.getElementById("zipset").style.visibility = "visible";
+        document.getElementById("csset").style.visibility = "visible";
+        var germany = document.getElementById("germany");
+        var us = document.getElementById("us");
+        var zip = document.getElementById("zip").value;
+        if (zip.addEventListener) {
+            germany.removeEventListener("click", checkButtons, false);
+            us.removeEventListener("click", checkButtons, false);
+            zip.removeEventListener("keyup", checkInput, false);
+        } else if (zip.detachEvent) {
+            germany.detachEvent("onclick", checkButtons);
+            us.detachEvent("onclick", checkButtons);
+            zip.detachEvent("onkeyup", checkInput);
+        }
+
+        return false;
+    }
+    return httpRequest;
+}
+
+// Requests location
+function getLocation() {
+    var zip = document.getElementById("zip").value;
+    if (!httpRequest) {
+        httpRequest = getRequestObject();
+    }
+    httpRequest.abort();
+    httpRequest.open("get", "https://api.zippopotam.us/" + countrySel + "/" + zip, true);
+    httpRequest.send(null);
+    httpRequest.onreadystatechange = displayData;
+}
+
+// Receives and displays data
+function displayData() {
+    if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+        var resultData = JSON.parse(httpRequest.responseText);
+        var city = document.getElementById("city");
+        var state = document.getElementById("state");
+        city.value = resultData.places[0]["state abbreviation"];
+        document.getElementById("zip").blur();
+        document.getElementById("csset").style.visibility = "visible";
+    }
+}
+
+var zip = document.getElementById("zip");
+if (zip.addEventListener) {
+    zip.addEventListener("keyup", checkInput, false);
+} else if (zip.attachEvent) {
+    zip.addEventListener("onkeyup", checkInput);
+}
+
+// Adds event listeners.
+if (us.addEventListener) {
+    germany.addEventListener("click", checkButtons, false);
+    us.addEventListener("click", checkButtons, false);
+} else if (us.attachEvent) {
+    germany.attachEvent("onclick", checkButtons);
+    us.attachEvent("onclick", checkButtons);
+}
